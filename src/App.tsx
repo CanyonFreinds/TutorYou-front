d25bc1e44264f3ecd4ac05002163a9a03dd2dec8
qkrdmstlr3 <qkrdmstlr3@naver.com>
2021-11-10 14:56:41 +0900

    [chore] react and emotinoJS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Div = styled.div`
+  color: blue;
+`;
+
+function App() {
+  return <Div>hello-world</Div>;
+}
+
+export default App;
