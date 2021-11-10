13a6487614cf9f91ca0b2719241dc5b6929645be
parkeunsik <qkrdmstlr3@naver.com>
2021-11-11 01:56:22 +0900

    [feat] [#4] Header Component UI구현
    - 공통 헤더로 사용될 컴포넌트의 ui를 구현
    - 조건에 따라 렌더링하는 부분은 로직을 구현하며 추가할 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './component/Header';
 import LoginPage from './page/Login';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route path="/login" component={LoginPage} />
       </Switch>
---
+++ src/component/Header/styled.ts
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import MaterialLoginIcon from '@mui/icons-material/Login';
+import MaterialAccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MaterialNotificationsIcon from '@mui/icons-material/Notifications';
+import MaterialLogoutIcon from '@mui/icons-material/Logout';
+import MaterialSupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 6rem;
+  padding: 0 3rem 0 2rem;
+
+  color: #ff6f61;
+  background-color: #f0f0f0;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const Navigator = styled.nav``;
+
+export const Link = styled.span`
+  margin: 0 2rem;
+  font-size: 2.1rem;
+  font-weight: bold;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+`;
+
+export const LoginIcon = styled(MaterialLoginIcon)`
+  margin: 0 0.7rem;
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const LogoutIcon = styled(MaterialLogoutIcon)`
+  margin: 0 0.7rem;
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const AccountCircleIcon = styled(MaterialAccountCircleIcon)`
+  margin: 0 0.7rem;
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const NotificationsIcon = styled(MaterialNotificationsIcon)`
+  margin: 0 0.7rem;
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
+
+export const SupervisorAccountIcon = styled(MaterialSupervisorAccountIcon)`
+  margin: 0 0.7rem;
+  font-size: 3.5rem;
+  cursor: pointer;
+`;
