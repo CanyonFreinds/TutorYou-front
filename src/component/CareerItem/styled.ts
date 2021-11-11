1b8002f7000573889ddb5b8df9428481f5af5cdd
parkeunsik <qkrdmstlr3@naver.com>
2021-11-11 13:45:34 +0900

    [feat] [#5] CareerItem Component 구현
    - 경력 사항을 입력받을 수 있는 컴포넌트 구현
    - 스토리 파일 작성

---
+++ src/component/CareerItem/styled.ts
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import MaterialDeleteIcon from '@mui/icons-material/Delete';
+
+export const Container = styled.div``;
+
+export const Title = styled.h2`
+  margin: 0;
+  font-size: 3rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+`;
+
+export const CareerInput = styled(Input)`
+  width: 500px;
+  margin-right: 1rem;
+  font-size: 1.6rem;
+`;
+
+export const SubmitButton = styled(Button)`
+  font-size: 1.8rem;
+`;
+
+export const ValueList = styled(List)``;
+
+export const ValueItem = styled(ListItem)`
+  font-size: 1.7rem;
+`;
+
+export const IconWrapper = styled(IconButton)``;
+
+export const DeleteIcon = styled(MaterialDeleteIcon)`
+  font-size: 2rem;
+`;
