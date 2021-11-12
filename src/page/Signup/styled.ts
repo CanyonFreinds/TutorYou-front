8ac5389fcaa66e96b16cbc117cbd4e8b08f9558b
yoo chang heon <dbckdgjs1130@naver.com>
2021-11-12 21:21:34 +0900

    [feat] [#7] Signup 페이지 구현
    UI만 구현

---
+++ src/page/Signup/styled.ts
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+import MaterialTextField from '@mui/material/TextField';
+import MaterialInput from '@mui/material/Input';
+import MaterialButton from '@mui/material/Button';
+
+export const TextField = styled(MaterialTextField)`
+  width: 500px;
+  font-size: 1rem;
+  align: left;
+  margin-top: 20px;
+`;
+
+export const Input = styled(MaterialInput)`
+  width: 500px;
+  font-size: 1rem;
+  align: left;
+  margin-top: 20px;
+`;
+export const Div = styled.div`
+  text-align: center;
+  margin-top: 2rem;
+  justify: center;
+`;
+
+export const Aligndiv = styled.div`
+  position: relative;
+  margin: 0 auto !important;
+  width: 500px !important;
+`;
+
+export const Label = styled.label`
+  font-size: 1.5rem;
+  font-weight: bold;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+export const CareerUl = styled.ul`
+  position: relative;
+`;
+
+export const CareerDiv = styled.div``;
+
+export const CareerItem = styled.li``;
+
+export const Button = styled(MaterialButton)`
+  width: 15rem;
+  height: 3rem;
+`;
