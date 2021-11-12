7f8e6a64af2ebd52e22a5124830ebad680777fa2
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-12 19:36:07 +0900

    [feat] [#3] 모집글 추가 Form 추가

--- src/page/RecruitmentWrite/index.tsx
+++ src/page/RecruitmentWrite/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import * as Style from './styled';
+import RecuritmentAddForm from '../../component/RecruitmentAddForm';
+
+type InitialValues = {[key: string]: string | number};
+
+const initialValues = {
+  categoryName: '',
+  content: '',
+  endDate: '',
+  postType: '',
+  startDate: '',
+  title: '',
+  totalStudentCount: 0,
+}
 
 function RecruitmentWrite() {
+  const [values, setValues] = useState<InitialValues>(initialValues);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <Style.Container>
-      RecruitmentWrite
+      <RecuritmentAddForm>
+        <></>
+      </RecuritmentAddForm>
     </Style.Container>
   );
 }
