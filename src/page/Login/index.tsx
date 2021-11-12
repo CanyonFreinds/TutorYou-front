97852e9897092f7f7037e00ee0086970951dbb65
yoo chang heon <dbckdgjs1130@naver.com>
2021-11-12 21:19:01 +0900

    [feat] [#7] Login 페이지 구현
    UI만 구현

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import GoogleIcon from '@mui/icons-material/Google';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import * as Style from './styled';
+
+const ariaLabel = { 'aria-label': 'description' };
 
 function Login() {
-  return <>login</>;
+  return (
+    <Style.Div>
+      <h1>로그인</h1>
+      <br />
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: 400, maxWidth: '100%' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Style.TextField
+          required
+          fullWidth
+          variant="standard"
+          label="e-mail"
+          placeholder="E-mail을 입력해주세요"
+          type="email"
+        />
+        <br />
+        <br />
+        <Style.TextField
+          required
+          variant="standard"
+          label="password"
+          placeholder="Password"
+          type="password"
+          inputProps={ariaLabel}
+        />
+      </Box>
+      <Style.Button size="large" variant="contained" href="#contained-buttons">
+        로그인
+      </Style.Button>
+      <Style.Button size="large" variant="contained" href="Signup">
+        회원가입하기
+      </Style.Button>
+      <br />
+
+      <Style.Button>
+        <GitHubIcon fontSize="large" htmlColor="black" />
+      </Style.Button>
+      <Style.Button>
+        <GoogleIcon fontSize="large" htmlColor="black" />
+      </Style.Button>
+    </Style.Div>
+  );
 }
 
 export default Login;
