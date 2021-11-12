1d1fe376f98222a82a57ede5cec3861fbce7020b
parkeunsik <qkrdmstlr3@naver.com>
2021-11-13 00:35:54 +0900

    [feat] [#18] 회원탈퇴 / 비번변경 기능 구현
    - 프로필 페이지에서 회원탈퇴 기능구현
    - 모달을 이용해서 비밀번호 변경 기능 구현

--- src/page/Profile/styled.ts
+++ src/page/Profile/styled.ts
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Box from '@mui/material/Box';
 
 export const Container = styled.main`
   width: 800px;
@@ -38,4 +40,29 @@ export const CareerItem = styled.li`
 
 export const ModifyButton = styled(Button)`
   font-size: 1.5rem;
+`;
+
+export const PasswordInput = styled(Input)`
+  width: 200px;
+  margin: 0.5rem 0;
+  font-size: 1.5rem;
+`;
+
+export const ChangeButton = styled(Button)`
+  font-size: 1.5rem;
+`;
+
+export const ModalBox = styled(Box)`
+  position: absolute;
+  padding: 1rem;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  border: 2px solid #000;
+  box-shadow: 24;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
