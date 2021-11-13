e31ad8b1eeda430bdbe9795618b258ce97c3e46a
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-13 13:17:24 +0900

    [feat] [#19] recruitment 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './component/Header';
 import LoginPage from './page/Login';
 import Recruitments from './page/Recruitments';
+import Recruitment from './page/Recruitment';
 import RecruitmentWrite from './page/RecruitmentWrite';
 import ProfilePage from './page/Profile';
 import TeachersPage from './page/Teachers';
@@ -10,7 +11,7 @@ import AdminPage from './page/Admin';
 import GroupPage from './page/Group';
 import SignupPage from './page/Signup';
 
-import { adminPath, loginPath, recruitmentsPath, recruitmentWritePath, profilePath, teaturesPath, groupPath, signupPath } from './Routes';
+import { adminPath, loginPath, recruitmentsPath, recruitmentPath, recruitmentWritePath, profilePath, teaturesPath, groupPath, signupPath } from './Routes';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path={loginPath} component={LoginPage} />
         <Route path={signupPath} component={SignupPage} />
         <Route path={recruitmentsPath} component={Recruitments} />
+        <Route path={recruitmentPath} component={Recruitment} />
         <Route path={recruitmentWritePath} component={RecruitmentWrite} />
         <Route path={teaturesPath} component={TeachersPage} />
         <Route path={profilePath} component={ProfilePage} />
---
+++ src/page/Recruitment/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './styled';
+
+function Recruitments() {
+  return (
+    <Style.Container>
+      d
+    </Style.Container>
+  );
+}
+
+export default Recruitments;
