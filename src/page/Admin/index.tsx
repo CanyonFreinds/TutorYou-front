9211e3a57be066c2bf9d2bd5a4d2e10555364820
parkeunsik <qkrdmstlr3@naver.com>
2021-11-13 17:49:12 +0900

    [feat] [#31] Admin 페이지 api 구현
    - 관리자 페이지에서 블랙리스트리스트를 가져오고 추가/제거하는 api구현

--- src/page/Admin/index.tsx
+++ src/page/Admin/index.tsx
@@ -1,51 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import * as Style from './styled';
-import BlackListCard, { InfoType } from '../../component/BlackListCard';
+import BlackListCard from '../../component/BlackListCard';
+import { getBlackListAPI, getTeacherListAPI, changeTeacherBanStateAPI, TeacherAdminType } from '../../api/admin';
+
+type TabType = 'teacher' | 'blacklist';
 
 function Admin() {
-  const [whiteList] = useState<InfoType[]>([]);
-  const [blackList] = useState<InfoType[]>([]);
+  const [tab, setTab] = useState<TabType>('teacher');
+  const [list, setList] = useState<TeacherAdminType[]>([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const loader = useRef(null);
 
-  const addToBlackList = () => {};
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setPageNumber(pageNumber + 1);
+    }
+  }, []);
 
-  const removeFromBlackList = () => {};
+  const getBlackList = async () => {
+    const result = await getBlackListAPI({ pageNumber });
+    if (result) {
+      setList([...list, ...result.results]);
+    }
+  };
+
+  const getTeacherList = async () => {
+    const result = await getTeacherListAPI({ pageNumber });
+    if (result) {
+      setList([...list, ...result.results]);
+    }
+  };
+
+  const toggleBlacklIST = async (teacherId: number) => {
+    const result = await changeTeacherBanStateAPI({ teacherId });
+    if (result) {
+      const filteredList = list.filter((item) => item.teacherId !== teacherId);
+      setList(filteredList);
+    }
+  };
+
+  const changeTab = (tab: TabType) => {
+    setList([]);
+    setPageNumber(0);
+    setTab(tab);
+  };
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+  }, [handleObserver]);
+
+  useEffect(() => {
+    if (tab === 'teacher') getTeacherList();
+    else getBlackList();
+  }, [tab, pageNumber]);
 
   return (
     <Style.Container>
-      <Style.Left>
-        <Style.TitleWrapper>
-          <Style.Title>선생님 리스트</Style.Title>
-          <Style.Dropdown defaultValue="random">
-            <option value="desc">내림차순</option>
-            <option value="asc">오름차순</option>
-            <option value="random">랜덤</option>
-          </Style.Dropdown>
-        </Style.TitleWrapper>
-        <Style.CardList>
-          {whiteList.map((info) => (
+      <Style.TitleWrapper>
+        <Style.Title onClick={() => changeTab('teacher')} selected={tab === 'teacher'}>
+          선생님 리스트
+        </Style.Title>
+        <Style.Title onClick={() => changeTab('blacklist')} selected={tab === 'blacklist'}>
+          블랙 리스트
+        </Style.Title>
+      </Style.TitleWrapper>
+      <Style.CardList>
+        {tab === 'teacher' &&
+          list.map((info) => (
             <Style.CardItem>
-              <BlackListCard info={info} clickButton={addToBlackList} />
+              <BlackListCard info={info} clickButton={toggleBlacklIST} />
             </Style.CardItem>
           ))}
-        </Style.CardList>
-      </Style.Left>
-      <Style.Right>
-        <Style.TitleWrapper>
-          <Style.Title>블랙 리스트</Style.Title>
-          <Style.Dropdown defaultValue="random">
-            <option value="desc">내림차순</option>
-            <option value="asc">오름차순</option>
-            <option value="random">랜덤</option>
-          </Style.Dropdown>
-        </Style.TitleWrapper>
-        <Style.CardList>
-          {blackList.map((info) => (
-            <Style.CardItem>
-              <BlackListCard info={info} clickButton={removeFromBlackList} />
-            </Style.CardItem>
-          ))}
-        </Style.CardList>
-      </Style.Right>
+      </Style.CardList>
+      <div ref={loader} />
     </Style.Container>
   );
 }
