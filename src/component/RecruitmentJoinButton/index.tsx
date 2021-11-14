1cc4a5f306b24b2ae135199d1879b946d460bc5c
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-14 14:23:26 +0900

    [feat] [#56] 그룹 참가 버튼 구현

---
+++ src/component/RecruitmentJoinButton/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import * as Style from './styled';
+
+interface RecruitmentEditButtonProps {
+  onClick: () => Promise<void>;
+}
+
+function RecruitmentJoinButton({ onClick }: RecruitmentEditButtonProps) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <Style.TextTooltip title="그룹 참가">
+        <Style.Container onClick={handleOpen} color="primary">
+          <Style.Icon />
+        </Style.Container>
+      </Style.TextTooltip>
+      <Style.MUIModal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Style.MUIBox>
+          <Style.Text id="modal-modal-title" variant="h4">
+            그룹에 참가하시겠습니까?
+          </Style.Text>
+          <Style.ButtonContainer>
+            <Style.MUIButton onClick={onClick} variant="contained" color="primary">참가</Style.MUIButton>
+            <Style.MUIButton onClick={handleClose} variant="contained" color="inherit">취소</Style.MUIButton>
+          </Style.ButtonContainer>
+        </Style.MUIBox>
+      </Style.MUIModal>
+    </>
+  );
+}
+
+export default RecruitmentJoinButton;
---
+++ src/component/RecruitmentJoinButton/styled.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+import Fab from '@mui/material/Fab';
+import GroupIcon from '@mui/icons-material/Group';
+import Tooltip from '@mui/material/Tooltip';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+
+export const Container = styled(Fab)`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin: 3rem;
+`;
+
+export const Icon = styled(GroupIcon)`
+  color: white;
+`;
+
+export const TextTooltip = styled(Tooltip)`
+`;
+
+export const Text = styled(Typography)`
+`;
+
+export const MUIModal = styled(Modal)`
+`;
+
+export const MUIBox = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  border-radius: 0.5rem;
+  background-color: white;
+`;
+
+export const ButtonContainer = styled(Box)`
+  display: flex;
+  margin-top: 2rem;
+  column-gap: 3rem;
+`;
+
+export const MUIButton = styled(Button)`
+  width: 10rem;
+  font-size: 1.8rem;
+  height: 3rem;
+`;
