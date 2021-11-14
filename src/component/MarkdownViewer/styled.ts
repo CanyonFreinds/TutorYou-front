97dbf18f6fedbedc9755fd67bae70b7bd56ddec4
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-14 13:38:40 +0900

    [feat] [#52] 게시글 이미지 호버 애니메이션 추가

--- src/component/MarkdownViewer/styled.ts
+++ src/component/MarkdownViewer/styled.ts
@@ -2,4 +2,14 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   font-size: 1.8rem;
+  img {
+    max-width: 100rem;
+    transition: all 0.3s ease;
+    border: 1px solid #fefefe;
+
+    :hover {
+      transform: scale(1.005);
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
+  }
 `;
